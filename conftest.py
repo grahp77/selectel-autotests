0e7d4295f1dac2f3590bcac4668ed039f4d28543
beulahsi <ehdu-y@yandex.ru>
2026-02-15 11:16:29 +0300

    clear

--- conftest.py
+++ conftest.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def driver():
-    """Фикстура для запуска и закрытия браузера"""
+    """Фикстура для браузера"""
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=1920,1080")
